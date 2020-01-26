0d99bd9d897250a67bcd26a0b668d9f5ae646ffe
creative-think <creative_think@163.com>
2020-01-26 21:20:24 +0800

    添加boost框架

---
+++ cmake-build-debug/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/creative/Documents/code/work/googletest
+# Build directory: /Users/creative/Documents/code/work/cmake-build-debug/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
